dd0e3ff194683d3dbd3b6ccfe687d72aa04f878a
belasco <danbelasco@yahoo.co.uk>
2013-03-07 15:01:09 +0100

    Catch no args error
    Add a check for argument (the name of the database to be created)
    and helpful message if user ran script without

--- extras/create_db.sh
+++ extras/create_db.sh
@@ -1,4 +1,11 @@
 #! /bin/sh
 
+if [ $# -ne 1 ]
+then 
+    echo "Please pass a database name to the script"
+    echo "e.g. create_db.sh newdb.sqlite"
+    exit 2
+fi
+
 spatialite $1 ".read create_db.sql utf-8"
 spatialite $1 ".read insert_citydefs.sql utf-8"
