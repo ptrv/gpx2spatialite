5191b278ec72347bb776f5d2c0a268b0c1c03d2f
ptrv <mail@petervasil.net>
2013-03-19 10:31:55 +0100

    Improve extras/create_db.sh script
    It is now possible to call script from other directories than extras

--- extras/create_db.sh
+++ extras/create_db.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
 if [ $# -ne 1 ]
-then 
+then
     echo "Please pass a database name to the script"
     echo "e.g. create_db.sh newdb.sqlite"
     exit 2
 fi
-
-spatialite $1 ".read create_db.sql utf-8"
-spatialite $1 ".read insert_citydefs.sql utf-8"
+# the dir from which the script is called
+DIR="$( dirname "$0" )"
+spatialite $1 ".read $DIR/create_db.sql utf-8"
+spatialite $1 ".read $DIR/insert_citydefs.sql utf-8"
